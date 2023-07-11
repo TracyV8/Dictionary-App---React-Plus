3e4009448d363fc84846c8995c57af49afb4e9c0
TracyV8 <123339740+TracyV8@users.noreply.github.com>
2023-07-11 10:43:57 +0800

    Title Changed, button added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        Dictionary
+        <a href="#" className="btn btn-primary shadow">
+          Search
+        </a>
+      </header>
     </div>
   );
 }
