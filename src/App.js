eebd434f017ddec855da648ac3c0db59776ea0c3
TracyV8 <123339740+TracyV8@users.noreply.github.com>
2023-07-11 11:23:08 +0800

    Added form for word search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Dictionary
-        <a href="#" className="btn btn-primary shadow">
-          Search
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This App was coded by Tracy Vonk and is{" "}
+          <a
+            href="https://github.com/TracyV8/Dictionary-App---React-Plus"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
